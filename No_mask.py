fixed phi coordinate's calculation

No_mask.py
@@ -87,9 +87,9 @@ def no_mask_fields(II1,II2,II3,lambda1,I0,beta,polarization,zsteps,rsteps,field_
     zz=ztotalsteps + int(np.rint(zp0/z_field_of_view*2*ztotalsteps))  #zz signals to the row of kz=kz0 in each II
     for xx in range(x2):
         for yy in range(y2):
-            xcord=xx - np.rint(2*rtotalsteps /np.sqrt(2))/2#not sure of multipliing by 2 and dividing by 2 outside the int, i thought it was to be sure to get the 0,0 at xx=np.rint(2*rtotalsteps /np.sqrt(2))/2
-            ycord=yy - np.rint(2*rtotalsteps /np.sqrt(2))/2
-            phip,rp=cart2pol(xcord+1,ycord+1)
+            xcord=xx - np.rint(2*rtotalsteps /2**0.5)/2#not sure of multipliing by 2 and dividing by 2 outside the int, i thought it was to be sure to get the 0,0 at xx=np.rint(2*rtotalsteps /np.sqrt(2))/2
+            ycord=-yy + np.rint(2*rtotalsteps /2**0.5)/2-1
+            phip,rp=cart2pol(xcord,ycord)#nuevamente el +1 es para no tener problemas
             rp=int(np.rint(rp))
             exx2[yy,xx]=-a1*1j*(II1[zz,rp]+np.cos(2*phip)*II3[zz,rp])
             eyx2[yy,xx]=-a1*1j*(np.sin(2*phip)*II3[zz,rp])

VPP.py
@@ -100,8 +100,8 @@ def VPP_fields(II1,II2,II3,II4,II5,lambda1,I0,beta,polarization,zsteps,rsteps,fi
     for xx in range(x2):
         for yy in range(y2):
             xcord=xx - np.rint(2*rtotalsteps /2**0.5)/2#not sure of multipliing by 2 and dividing by 2 outside the int, i thought it was to be sure to get the 0,0 at xx=np.rint(2*rtotalsteps /np.sqrt(2))/2
-            ycord=yy - np.rint(2*rtotalsteps /2**0.5)/2
-            phip,rp=cart2pol(xcord+1,ycord+1)#nuevamente el +1 es para no tener problemas
+            ycord=-yy + np.rint(2*rtotalsteps /2**0.5)/2-1
+            phip,rp=cart2pol(xcord,ycord)#nuevamente el +1 es para no tener problemas
             rp=int(np.rint(rp))
             exx2[yy,xx]=a1*(II1[zz,rp]*np.exp(1j*phip) - 0.5*II2[zz,rp]*np.exp(-1j*phip) + 0.5*II3[zz,rp]*np.exp(3j*phip))
             eyx2[yy,xx]=- 0.5*a1*1j*(II2[zz,rp]*np.exp(- 1j*phip) + II3[zz,rp]*np.exp(3j*phip))

plot_esp.py
@@ -25,7 +25,7 @@ def plot_XZ_XY(ex1,ey1,ez1,ex2,ey2,ez2,zsteps,rsteps,field_of_view_x, field_of_v
     fig = plt.figure(num=str(figure_name)+': Intensidad',figsize=(16, 8))
     spec = fig.add_gridspec(ncols=3, nrows=2)
     ax1 = fig.add_subplot(spec[0, 0])
-    ax1.set_title('Intensidad en y=0')
+    ax1.set_title('Intensidad en plano XZ')
     pos=ax1.imshow(Ifield_xz,extent=[-rmax,rmax,-zmax,zmax], interpolation='none', aspect='equal')
     ax1.set_xlabel('x (nm)')
     ax1.set_ylabel('z (nm)')
@@ -41,7 +41,7 @@ def plot_XZ_XY(ex1,ey1,ez1,ex2,ey2,ez2,zsteps,rsteps,field_of_view_x, field_of_v
 
     xmax=field_of_view_x/2
     ax2 = fig.add_subplot(spec[0, 1])
-    ax2.set_title('Intensidad en z=0')
+    ax2.set_title('Intensidad en plano XY')
     pos2=ax2.imshow(Ifield_xy,extent=[-xmax,xmax,-xmax,xmax],interpolation='none', aspect='auto')
     cbar2=fig.colorbar(pos2, ax=ax2)
     ax2.set_xlabel('x (nm)')
@@ -53,13 +53,13 @@ def plot_XZ_XY(ex1,ey1,ez1,ex2,ey2,ez2,zsteps,rsteps,field_of_view_x, field_of_v
     Ifield_axis=Ifield_xy[int(x2/2),:]
     axis=np.linspace(-xmax,xmax,x2)
     ax3 = fig.add_subplot(spec[0, 2])
-    ax3.set_title('Intensidad en z=0, y=0')
+    ax3.set_title('Intensidad en eje X')
     ax3.plot(axis,Ifield_axis)
     ax3.set_xlabel('x (nm)')
     ax3.set_ylabel('Intensidad  (kW/cm\u00b2)')  
     
     ax4 = fig.add_subplot(spec[1, 1])
-    ax4.set_title('Polarización en z=0')
+    ax4.set_title('Polarización en plano XY')
     pos4=ax4.imshow(Ifield_xy,extent=[-xmax,xmax,-xmax,xmax],interpolation='none', aspect='auto',alpha=0.5)
     cbar4=fig.colorbar(pos4, ax=ax4)
     cbar4.ax.set_ylabel('Intensidad (kW/cm\u00b2)')